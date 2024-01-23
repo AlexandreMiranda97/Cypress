f29aef344939f567a96fee71981468d551619574
Alexandre Miranda <alexmiranda97.am@gmail.com>
2024-01-23 20:56:20 -0300

    Code refreshing

--- cypress/e2e/integration/My_first_test.cy.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress"/>
-
-it('Google test', function(){
-
-    cy.visit('https://www.google.com')
-    cy.get('#APjFqb').type('JavaScript')
-    cy.get('.FPdoLc > center > .gNO89b').click()
-
-})
