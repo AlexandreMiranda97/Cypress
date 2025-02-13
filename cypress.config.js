f2099c267fb238b116b3d1794776389f660528e7
Alexandre Miranda <alexmiranda97.am@gmail.com>
2025-02-12 23:58:22 -0300

    Teste inicial de preenchimento de formulário no site Parabank.

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "xzkgtr",
   e2e: {
-    baseUrl: 'https://www.saucedemo.com',
-    // chromeWebSecurity: false,
-
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/3-tests/login-test.cy.js
+++ cypress/e2e/3-tests/login-test.cy.js
@@ -1,8 +1,39 @@
-describe('Login test', function(){
-    it('Success login', () => {
-        cy.visit('/')
-        cy.get('#user-name').type('standard_user')
-        // cy.get('id="password"').type('secret_sauce')
-        // cy.get('id="login-button"').click()
-    });
+import { faker } from '@faker-js/faker';
+
+const randomFName = faker.person.firstName();
+const randomLName = faker.person.lastName();
+const fakeAdress = faker.location.streetAddress();
+const fakeCity = faker.location.city();
+const fakeState = faker.location.state();
+const fakeZipCode = faker.location.zipCode();
+const fakeNumber = faker.phone.number({ style: 'national' });
+const fakeSSN = faker.string.numeric(9);
+const fakeUsername = faker.internet.username();
+const fakePassword = faker.internet.password();
+
+describe('Login test', () => {
+    beforeEach(() => { 
+        cy.viewport(1024, 768)
+        cy.visit('https://parabank.parasoft.com/parabank/index.htm', {timeout: 30000})
+       
+    })
+
+    it('Realizar cadastro no site Parabank', () => {
+        cy.get('.logo')
+        cy.get('a').contains('Register').click()
+        cy.get('.title').contains('Signing up is easy!')
+        cy.get('#customerForm').find('[name="customer.firstName"]').type(randomFName)
+        cy.get('#customerForm').find('[id="customer.lastName"]').type(randomLName)
+        cy.get('#customerForm').find('[id="customer.address.street"]').type(fakeAdress)
+        cy.get('#customerForm').find('[id="customer.address.city"]').type(fakeCity)
+        cy.get('#customerForm').find('[id="customer.address.state"]').type(fakeState)
+        cy.get('#customerForm').find('[id="customer.address.zipCode"]').type(fakeZipCode)
+        cy.get('#customerForm').find('[id="customer.phoneNumber"]').type(fakeNumber)
+        cy.get('#customerForm').find('[id="customer.ssn"]').type(fakeSSN)
+        cy.get('#customerForm').find('[id="customer.username"]').type(fakeUsername)
+        cy.get('#customerForm').find('[id="customer.password"]').type(fakePassword)
+        cy.get('#customerForm').find('[id="repeatedPassword"]').type(fakePassword)
+        cy.get('[colspan="2"] > .button').click()
+        cy.contains('Your account was created successfully. You are now logged in.')
+    })
 })
