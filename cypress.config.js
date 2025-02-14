3a1f92d31abdf9bb709d994db36808622fafda3a
Alexandre Miranda <alexmiranda97.am@gmail.com>
2025-02-14 19:28:57 -0300

    Adição de configurações do browser Chrome

--- cypress.config.js
+++ cypress.config.js
@@ -10,6 +10,16 @@ module.exports = defineConfig({
 
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on("before:browser:launch" === "chrome", (browser = {}, launchOptions) => {
+        if (browser.name === "chrome") {
+          launchOptions.args.push(
+            "--disable-save-password-bubble",
+            "--disable-password-generation",
+            "--disable-popup-blocking"
+          )
+        }
+        return launchOptions;
+      })
     },
   },
 });
